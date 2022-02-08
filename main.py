16bb0e4868d536cfeb00cf5368ff35dd60efd938
DmtkD <pavlo.to474@gmail.com>
2022-02-08 10:33:22 +0200

    Last update

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ class Fish(FishInfo):
         self.age_in_months = age_in_months
         self.weight = weight
 
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return "Fish info, name: {0}, origin: {1}, price={2}".format(self.name, self.origin, self.price_in_uah_per_kilo)
+
 
 class FishBox(FishInfo):
 
@@ -39,8 +45,6 @@ class FishShop:
     def __init__(self) -> None:
         self.frozen_fish_boxes: dict[str: list[FishBox]] = {}
         self.fresh_fishes: dict[str: list[Fish]] = {}
-        self.frozen_fish_by_price: List[tuple[str, float]] = []
-        self.fresh_fish_by_price: List[tuple[str, float]] = []
         self.fish_names_by_price: List[tuple[str, bool, float]] = []
 
     @singledispatchmethod
@@ -49,76 +53,57 @@ class FishShop:
 
     @add_fish.register                                                        # ПАСХАЛКА "Тут був автор(-ка) коту/коду"
     def _add_fish(self, fish_box: FishBox) -> None:
-        if not self.frozen_fish_boxes:
-            self.frozen_fish_boxes[fish_box.name] = [fish_box]
-        else:
-            for key in self.frozen_fish_boxes:
-                count: int = 0
-                if key == fish_box.name:
-                    self.frozen_fish_boxes[fish_box.name].append(fish_box)
-                else:
-                    count += 1
-                    if count == len(self.fresh_fishes):
-                        self.frozen_fish_boxes[fish_box.name] = [fish_box]
-                        break
+        self.fresh_fishes.setdefault(fish_box.name, []).append(fish_box)
 
     @add_fish.register
     def _add_fish(self, fish: Fish) -> None:
-        if not self.fresh_fishes:
-            self.fresh_fishes[fish.name] = [fish]
-        else:
-            for key in self.fresh_fishes:
-                count: int = 0
-                if key == fish.name:
-                    self.fresh_fishes[fish.name].append(fish)
-                else:
-                    count += 1
-                    if count == len(self.fresh_fishes):
-                        self.fresh_fishes[fish.name] = [fish]
-                        break
+        self.fresh_fishes.setdefault(fish.name, []).append(fish)
 
     def get_frozen_fish_names_sorted_by_price(self) -> List[tuple[str, float]]:
+        frozen_fish_by_price = []
         for key in self.frozen_fish_boxes:
             for i in range(len(self.frozen_fish_boxes[key])):
-                self.frozen_fish_by_price.append((self.frozen_fish_boxes[key][i].name,
-                                                  self.frozen_fish_boxes[key][i].price_in_uah_per_kilo))
-        self.frozen_fish_by_price.sort(key=lambda price: price[1])
-        return self.frozen_fish_by_price
+                frozen_fish_by_price.append((self.frozen_fish_boxes[key][i].name,
+                                             self.frozen_fish_boxes[key][i].price_in_uah_per_kilo))
+        frozen_fish_by_price.sort(key=lambda price: price[1])
+        return frozen_fish_by_price
 
     def get_fresh_fish_names_sorted_by_price(self) -> List[tuple[str, float]]:
+        fresh_fish_by_price = []
         for key in self.fresh_fishes:
             for i in range(len(self.fresh_fishes[key])):
-                self.fresh_fish_by_price.append((self.fresh_fishes[key][i].name,
-                                                 self.fresh_fishes[key][i].price_in_uah_per_kilo))
-        self.fresh_fish_by_price.sort(key=lambda price: price[1])
-        return self.fresh_fish_by_price
+                fresh_fish_by_price.append((self.fresh_fishes[key][i].name,
+                                            self.fresh_fishes[key][i].price_in_uah_per_kilo))
+        fresh_fish_by_price.sort(key=lambda price: price[1])
+        return fresh_fish_by_price
 
     def get_fish_names_sorted_by_price(self) -> List[tuple[str, bool, float]]:
+        fish_names_by_price = []
         for key in self.frozen_fish_boxes:
             for i in range(len(self.frozen_fish_boxes[key])):
-                self.fish_names_by_price.append((self.frozen_fish_boxes[key][i].name,
-                                                 self.frozen_fish_boxes[key][i].is_alive,
-                                                 self.frozen_fish_boxes[key][i].price_in_uah_per_kilo))
+                fish_names_by_price.append((self.frozen_fish_boxes[key][i].name,
+                                            self.frozen_fish_boxes[key][i].is_alive,
+                                            self.frozen_fish_boxes[key][i].price_in_uah_per_kilo))
         for key in self.fresh_fishes:
             for i in range(len(self.fresh_fishes[key])):
-                self.fish_names_by_price.append((self.fresh_fishes[key][i].name, True,
-                                                 self.fresh_fishes[key][i].price_in_uah_per_kilo))
-        self.fish_names_by_price.sort(key=lambda price: price[2])
-        return self.fish_names_by_price
+                fish_names_by_price.append((self.fresh_fishes[key][i].name, True,
+                                            self.fresh_fishes[key][i].price_in_uah_per_kilo))
+        fish_names_by_price.sort(key=lambda price: price[2])
+        return fish_names_by_price
 
     def sell_fish(self, fish_name: str, weight: float, is_fresh: bool) -> None:
         pass
 
     def cast_out_old_fish(self) -> None:
         for key in self.fresh_fishes:
+            k: int = 0
             for i in range(len(self.fresh_fishes[key])):
-                k: int = 0
                 if self.fresh_fishes[key][i-k].due_data == date.today():
                     self.fresh_fishes[key].pop(i-k)
                     k += 1
         for key in self.frozen_fish_boxes:
+            k: int = 0
             for i in range(len(self.frozen_fish_boxes[key])):
-                k: int = 0
                 if self.frozen_fish_boxes[key][i-k] == date.today():
                     self.frozen_fish_boxes[key].pop(i-k)
                     k += 1
@@ -140,3 +125,13 @@ class Buyer:
 
     def check_money_for_buying(self, price: float) -> bool:
         pass
+
+
+fishes = Fish(10, 20, "Oleg", "Ukraine", 200, [2020, 12, 12], [2030, 12, 31])
+fishes2 = Fish(10, 20, "Oleg", "Ukraine", 150, [2021, 12, 12], [2030, 12, 31])
+fishes3 = Fish(10, 20, "Ol", "Ukraine", 170, [2021, 12, 12], [2030, 12, 31])
+shop = FishShop()
+shop.add_fish(fishes)
+shop.add_fish(fishes2)
+shop.add_fish(fishes3)
+print(shop.fresh_fishes)
